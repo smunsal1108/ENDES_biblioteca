b98fd359307fc79a8ec8fc087dbbe67c5458e99d
smunsal1108 <smunsal1108@g.educaand.es>
2024-04-16 20:04:45 +0200

    Autor.java actualicion v1

--- Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
+++ Codigo/biblioteca/src/main/java/com/endes/biblioteca/Autor.java
@@ -1,8 +1,51 @@
 package com.endes.biblioteca;
 
+import java.util.List;
+
+/**
+ * Representa a un autor de libros dentro de un sistema de gestión de biblioteca.
+ * Contiene información personal del autor y una lista de libros que ha escrito.
+ * 
+ * @author Saul
+ * @version 1.0
+ */
+
 public class Autor {
     private String name;
     private String biography;
+    private List<Libro> librosEscritos;
+	
+    /**
+     * Obtiene la lista de libros escritos por el autor
+     * 
+     * @return Una lista de objetos {@link Libro} que representa los libros escritos por el autor.
+     */
+    
+    public List<Libro> getLibrosEscritos() {
+    	return librosEscritos;
+    }
+    
+    public Autor(String name, String biography) {
+		this.name = name;
+		this.biography = biography;
+	}
 
-    // Constructor, getters y setters aquí
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+    
+    
 }
--- Codigo/biblioteca/src/main/java/com/endes/biblioteca/Libro.java
+++ Codigo/biblioteca/src/main/java/com/endes/biblioteca/Libro.java
@@ -6,6 +6,37 @@ public class Libro {
     private String isbn;
     private String title;
     private List<Autor> autores;
+	
+    public Libro(String isbn, String title, List<Autor> autores) {
+		this.isbn = isbn;
+		this.title = title;
+		this.autores = autores;
+	}
 
-    // Constructor, getters y setters aquí
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
+
+    
+    
 }
